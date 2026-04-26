998175d64536d68ced6cf5472b3f0feafe1871a6
Saenko Dmitry <dmitryssaenko@gmail.com>
2026-04-26 18:58:43 +0300

    loading env file

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
+
 	"markdown-viewer/internal/server"
 	"markdown-viewer/internal/session"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	if err := session.RestoreFromDisk(); err != nil {
 		log.Printf("Failed to restore sessions: %v", err)
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module markdown-viewer
 
 go 1.24
 
-require github.com/gomarkdown/markdown v0.0.0-20260417124207-7d523f7318df // indirect
+require (
+	github.com/gomarkdown/markdown v0.0.0-20260417124207-7d523f7318df
+	github.com/joho/godotenv v1.5.1
+)
